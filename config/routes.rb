efcc99f20507c33f1df42dc27c0ae135fc9c3eed
Danilson Bengui <danilsonjuniorbengui23@gmail.com>
2023-02-28 20:16:59 +0100

    meu segundo commit

--- config/routes.rb
+++ config/routes.rb
@@ -18,3 +18,10 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "articles#index"
 end
+
+Rails.application.routes.draw do 
+  root "articles#index"
+
+  get "/articles", to: "articles#index"
+  get "/articles/:id", to: "articles#show"
+end
